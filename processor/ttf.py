bda0667fc5072414e627759b3310da131e3f3def
Leo Lou <qibinlou@hotmail.com>
2013-06-18 00:00:08 +0800

    hello

--- processor/ttf.py
+++ processor/ttf.py
@@ -7,10 +7,10 @@ val={}
 yo=file("input.yo")
 
 while True:
-	line=yo.readline()
-	if	line=="":
-		break
-	word.append(line)
+    line=yo.readline()
+    if    line=="":
+        break
+    word.append(line)
 
 Y86API.initY86(word)
 
@@ -19,7 +19,8 @@ Y86API.exeY86()
 val,memory = Y86API.getY86()
 
 while val["end"]==False:
-	Y86API.setY86(val,memory)
-	Y86API.exeY86()
-	val,memory=Y86API.getY86()
-	print("@@@@@@@@@@@@@@@@@@@@@@@@")
+    Y86API.setY86(val,memory)
+    Y86API.exeY86()
+    val,memory=Y86API.getY86()
+    # print "fuck",len(val),len(memory)
+    print("@@@@@@@@@@@@@@@@@@@@@@@@")
